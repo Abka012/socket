5d051c99a7fda2a8d2696e0d6c13a50921545c34
Abka Ferguson <fergusonabka012@gmail.com>
2024-12-26 20:04:27 -0500

    socket created

--- main.c
+++ main.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(){
+// run python3 -m http.server 8080 in the terminal
 
-  int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+int main() {
+    int socketFD;
+    struct sockaddr_in address;
 
-  char* ip = "142.250.188.46";
-  struct sockaddr_in address;
-  address.sin_family = AF_INET;
-  address.sin_port = 80;
-  inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
-    
-  int result = connect(socketFD, &address, sizeof(address));
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
 
-  if(result == 0) printf("Connection was succesful\n");
-  
-  return 0;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(8080);
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int result = connect(socketFD, (const struct sockaddr *)&address, sizeof(address));
+    if (result < 0) {
+        perror("Connection failed");
+        return 1;
+    }
+
+    printf("Connected successfully!\n");
+    return 0;
 }
